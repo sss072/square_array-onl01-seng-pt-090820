2e34e9e84c4ec1be6dbc5434bd9bd48007ee26c8
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-20 23:45:21 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   new_array = []
-  array.collect do |number|
-    number ** 2 
+  array.each do |number|
+    new_array << number ** 2 
      
   end
-   
+  new_array 
 end
